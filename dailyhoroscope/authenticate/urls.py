7c3e05fbc00ef1c5539ddff648080e7cdd92d3fc
Alvin <57538990+TheAXPerience@users.noreply.github.com>
2023-09-12 17:21:49 -0700

    Update urls.py: add LogoutView to urls

--- dailyhoroscope/authenticate/urls.py
+++ dailyhoroscope/authenticate/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login', views.LoginView.as_view(), name='login_view'),
     path('user', views.UserView.as_view(), name='user_view'),
     path('change-password', views.ChangePasswordView.as_view(), name='change_password_view'),
-    path('refresh', views.RefreshView.as_view(), name='refresh_view')
+    path('refresh', views.RefreshView.as_view(), name='refresh_view'),
+    path('logout', views.LogoutView.as_view(), name='logout_view'),
 ]
