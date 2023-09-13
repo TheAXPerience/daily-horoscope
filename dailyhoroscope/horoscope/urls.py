21e7241ff5b369e6be7dba5454e3135799c6f60a
Alvin <57538990+TheAXPerience@users.noreply.github.com>
2023-09-13 16:16:58 -0700

    Update urls.py: preliminary urls

--- dailyhoroscope/horoscope/urls.py
+++ dailyhoroscope/horoscope/urls.py
@@ -2,4 +2,9 @@ from django.urls import path
 from horoscope import views 
 from rest_framework.urlpatterns import format_suffix_patterns
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.HoroscopeView.as_view(), 'horoscope_view'),
+    path('<int:hid>', views.SingularHoroscopeView.as_view(), 'singular_horoscope_view'),
+    path('report/<int:hid>', views.ReportHoroscopeView.as_view(), 'report_horoscope_view'),
+    path('user/<str:username>', views.UserHoroscipeView.as_view(), 'user_horoscope_view'),
+]
