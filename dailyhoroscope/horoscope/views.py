9d6044ebcc2b3bf3f80ca4c88d8591e070b475ce
Alvin <57538990+TheAXPerience@users.noreply.github.com>
2023-09-13 16:09:59 -0700

    Update views.py: setting up all endpoints

--- dailyhoroscope/horoscope/views.py
+++ dailyhoroscope/horoscope/views.py
@@ -9,3 +9,44 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 # Create your views here.
+
+# POST: create horoscope
+class HoroscopeView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        pass
+
+# GET: read a singular horoscope
+# PUT/PATCH: update horoscope
+# DELETE: delete horoscope
+class SingularHoroscopeView(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get(self, request, hid):
+        pass
+
+    def put(self, request, hid):
+        pass
+
+    def patch(self, request, hid):
+        return self.put(request, hid)
+
+    def delete(self, request, hid):
+        pass
+
+# GET: daily horoscopes (of a given day)
+class DailyHoroscopeView(APIView):
+    def get(self, request, date=None):
+        # NOTE: if date is None, use current date
+        pass
+
+# GET: all horoscopes (paginated) of a user
+class HoroscopesOfUserView(APIView):
+    def get(self, request, username):
+        pass
+
+# POST: report a horoscope for inappropriate content
+class ReportHoroscope(APIView):
+    def post(self, request, hid):
+        pass
